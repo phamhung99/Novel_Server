f7d0129c285707f22c2b2d0d68a887ff91f13541
Sơn Hoàng <60213798+SonHoang2@users.noreply.github.com>
2025-09-27 08:29:41 +0700

    feat(constants): update comic image generation value to 50

--- src/common/constants/app.constant.ts
+++ src/common/constants/app.constant.ts
@@ -2,7 +2,7 @@ import { LightningActionType } from '../enums/app.enum';
 
 export const LIGHTNING_VALUES: Record<LightningActionType, number> = {
     [LightningActionType.COMIC_STORY_GENERATION]: 10,
-    [LightningActionType.COMIC_IMAGE_GENERATION]: 3,
+    [LightningActionType.COMIC_IMAGE_GENERATION]: 50,
 };
 
 export const MAX_MSG_COUNT_PER_DAY = 3;
