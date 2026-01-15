10878689ff65f92c23d589c3391e5c399e235df4
Sơn Hoàng <60213798+SonHoang2@users.noreply.github.com>
2026-01-15 16:48:49 +0700

    feat(master-data): change record login from user info to master data

--- src/master-data/master-data.service.ts
+++ src/master-data/master-data.service.ts
@@ -1,17 +1,25 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { StoryService } from 'src/story/story.service';
 import { ERROR_MESSAGES } from 'src/common/constants/app.constant';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class MasterDataService {
-    constructor(private readonly storyService: StoryService) {}
+    constructor(
+        private readonly storyService: StoryService,
+        private readonly userService: UserService,
+    ) {}
 
     async getMasterData(userId: string): Promise<any> {
         if (!userId) {
             throw new BadRequestException(ERROR_MESSAGES.USER_ID_REQUIRED);
         }
 
+        const user = await this.userService.findById(userId, false);
+
         const categories = await this.storyService.getAllCategories();
+
+        await this.userService.recordDailyCheckInAndGrantBonus(user);
 
         return {
             categories,
